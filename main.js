5472e4584b8c480f53328714582b22aec514b832
Huỳnh Trọng Phúc <91412382+DEV-trongphuc@users.noreply.github.com>
2024-10-06 04:36:09 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
   const startBtn = document.querySelector(".start_btn");
   const exClaim = document.querySelector(".ex_claim");
-  const initialClaim = 0.0; // Giá trị khởi đầu của EXG
+  const points = document.querySelector(".ex_ponits > span");
+  const saveNumPoints = localStorage.getItem("points");
+  if (saveNumPoints) {
+    points.innerText = (saveNumPoints * 1).toLocaleString("en-US");
+  } else {
+    points.innerText = (500).toLocaleString("en-US");
+  }
   const earningRate = 10 / 3600; // Mỗi giây cộng dồn
+  const initialClaim = earningRate; // Giá trị khởi đầu của EXG
   let totalClaim = initialClaim;
-
   function startCountdown(endTime) {
     startBtn.classList.add("mining");
     const countdown = setInterval(() => {
       const now = new Date();
       const timeLeft = endTime - now;
-
       if (timeLeft <= 0) {
         clearInterval(countdown);
         localStorage.removeItem("endTime"); // Xóa thời gian khi đếm ngược xong
+        localStorage.setItem("claim", "1"); // Xóa thời gian khi đếm ngược xong
         startBtn.classList.remove("mining");
-        startBtn.innerText = "Starting";
+        startBtn.innerText = "CLAIM";
+        startBtn.classList.add("claim");
+        const claimBtn = document.querySelector(".claim");
+        claimBtn.addEventListener("click", () => {
+          handleClaim();
+        });
       } else {
         startBtn.innerText = formatTimeLeft(endTime);
         // Tính toán tổng số EXG dựa trên thời gian đã trôi qua
@@ -43,7 +54,19 @@ document.addEventListener("DOMContentLoaded", () => {
       "0"
     )}:${String(seconds).padStart(2, "0")}`;
   }
-
+  function handleClaim() {
+    const saveNumPoints = localStorage.getItem("points");
+    let numPoints;
+    if (saveNumPoints) {
+      numPoints = saveNumPoints * 1 + totalClaim;
+    } else {
+      numPoints = 500 + totalClaim;
+    }
+    points.innerText = numPoints.toLocaleString("en-US");
+    localStorage.removeItem("claim");
+    localStorage.setItem("points", numPoints.toString());
+    totalClaim = 0;
+  }
   function updateExClaim() {
     // Định dạng số với dấu . cho phần thập phân và dấu , cho phần ngàn
     exClaim.innerText =
@@ -54,21 +77,41 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   startBtn.addEventListener("click", function () {
-    const endTime = new Date(Date.now() + 24 * 60 * 60 * 1000); // Thời gian kết thúc sau 24 giờ
+    const endTime = new Date(Date.now() + 1 * 10 * 1000); // Thời gian kết thúc sau 24 giờ
     localStorage.setItem("endTime", endTime); // Lưu endTime vào localStorage
     startCountdown(endTime);
   });
 
   // Kiểm tra xem có endTime trong localStorage không
   const savedEndTime = localStorage.getItem("endTime");
-  if (savedEndTime) {
+  const saveClaim = localStorage.getItem("claim");
+  if (savedEndTime && !saveClaim) {
     const endTime = new Date(savedEndTime);
     if (endTime > new Date()) {
       startCountdown(endTime); // Bắt đầu đếm ngược nếu còn thời gian
     } else {
-      localStorage.removeItem("endTime"); // Xóa nếu thời gian đã hết
-      startBtn.innerText = "Starting";
+      totalClaim = initialClaim * 24 * 60 * 60;
+      updateExClaim(); // Cập nhật số tiền EXG mỗi giây
+      localStorage.removeItem("endTime"); // Xóa thời gian khi đếm ngược xong
+      localStorage.setItem("claim", "1"); // Xóa thời gian khi đếm ngược xong
+      startBtn.classList.remove("mining");
+      startBtn.innerText = "CLAIM";
+      startBtn.classList.add("claim");
+      const claimBtn = document.querySelector(".claim");
+      claimBtn.addEventListener("click", () => {
+        handleClaim();
+      });
     }
+  } else if (!savedEndTime && saveClaim) {
+    totalClaim = initialClaim * 24 * 60 * 60;
+    updateExClaim(); // Cập nhật số tiền EXG mỗi giây
+    startBtn.classList.remove("mining");
+    startBtn.innerText = "CLAIM";
+    startBtn.classList.add("claim");
+    const claimBtn = document.querySelector(".claim");
+    claimBtn.addEventListener("click", () => {
+      handleClaim();
+    });
   }
   const menu = document.querySelectorAll(".ex_navs > ul >li");
   const pages = document.querySelectorAll(".ex_box > div");
